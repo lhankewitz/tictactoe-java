6467067f134ceed0e01103b3df0a2f4f6a3b8cdf
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-19 15:02:25 +0200

    test env is running

---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class to ...
+ *
+ * @author lumiha
+ * @since 19/05/15.
+
+[]ToTest:
+ */
+public class TicTacToeTest {
+    @Test
+    public void testEnvFunctioning() {
+        assertThat("A", is("A") );
+    }
+}
