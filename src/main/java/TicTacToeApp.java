b7ecc3cbb6c885f815d3fb993ae246930896b007
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-06-10 10:41:33 +0200

    added grid output for game

--- src/main/java/TicTacToeApp.java
+++ src/main/java/TicTacToeApp.java
@@ -7,13 +7,16 @@
 public class TicTacToeApp {
 
     public static void main(String[] args) {
-        final TicTacToe ticTacToe = new TicTacToe();
+        Grid grid = new Grid();
+        final TicTacToe ticTacToe = new TicTacToe(grid);
 
         ticTacToe.setPlayerOne(1, 1);
         ticTacToe.setPlayerTwo(1, 2);
         ticTacToe.setPlayerOne(2, 2);
         ticTacToe.setPlayerTwo(2, 1);
         ticTacToe.setPlayerOne(3, 3);
+
+        System.out.println(new GridPrinter(grid).formatGrid());
 
         System.out.println("Winner is " + ticTacToe.getWinner());
     }
